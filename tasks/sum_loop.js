58bf1e305d54fa74bf54d32c81ec88d306e5df8d
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-20 10:09:34 +0300

    Added task: sum_loop(for, reduce)

---
+++ tasks/sum_loop.js
@@ -0,0 +1,34 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+/**
+ * Sums values in array by loop "for"
+ * @param {Array} arr Array of numbers
+ * @return {number} Sum of the numbers
+ */
+const sumLoop = ( arr ) => {
+    const newArr = Array.prototype.slice.call( arr );
+    let result = 0;
+
+    for ( let i = 0; i < newArr.length; i++ ) {
+        result += newArr[i];
+    }
+
+    return result;
+};
+
+/**
+ * Sums values in array by function "reduce"
+ * @param {Array} arr Array of numbers
+ * @return {number} Sum of the numbers
+ */
+const sumReduce = ( arr ) => {
+    const newArr = Array.prototype.slice.call( arr );
+    let result = newArr.reduce( ( curr, prev ) => {
+        return curr + prev;
+    } );
+
+    return result;
+};
+
+console.log( sumLoop( arr ) );
+console.log( sumReduce( arr ) );
