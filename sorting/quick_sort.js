efffaec3dcf92ed8466e0a58cdd52cc84476e9c9
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-20 12:33:11 +0300

    Added quick sort

---
+++ sorting/quick_sort.js
@@ -0,0 +1,16 @@
+const quickSort = ( arr ) => {
+    if ( arr < 2 ) return arr;
+
+    let pivot = arr[0];
+
+    let smallerPart = arr.slice( 1 ).filter( ( el ) => { return el <= pivot } );
+
+    let biggerPart = arr.slice( 1 ).filter( ( el ) => { return el > pivot } );
+
+    return quickSort( smallerPart ).concat( [pivot], quickSort( biggerPart ) );
+};
+
+
+let arr = [5, 3, 6, 2, 10];
+
+console.log( quickSort(arr) );
