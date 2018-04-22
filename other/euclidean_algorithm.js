c9e375ae7bf98736fd9090c1dc8f3c19484e1e4b
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-22 15:05:35 +0300

    Added Euclidean algorithm

---
+++ other/euclidean_algorithm.js
@@ -0,0 +1,23 @@
+/**
+ * Recursive function of Euclidean algorithm
+ *
+ * @param {number} a first number
+ * @param {number} b second number
+ *
+ * @return {number} GCD (greatest common divisor)
+ */
+let getGCD = ( a, b ) => {
+    if ( a === 0 ) {
+        return b;
+    } else if ( b === 0 ) {
+        return a;
+    } else {
+        let remainder = a % b;
+        return getGCD( b, remainder );
+    }
+};
+
+const a = 1680;
+const b = 640;
+
+console.log( getGCD( a, b ) );
