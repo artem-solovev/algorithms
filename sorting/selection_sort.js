c093181ef11dfca9983c425b2a699a5f89d60e11
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-22 13:29:07 +0300

    Added unified function

--- sorting/selection_sort.js
+++ sorting/selection_sort.js
@@ -1,35 +1,51 @@
 /**
- * Finds smallest element of an aray
+ * Finds smallest/greatest element of an array
+ *
  * @param {Array} arr array for searching
- * @return {number} index of the smallest element in array
+ * @param {boolean} mod TRUE for smallest or FALSE for greatest value
+ *
+ * @return {number} index of smallest/greatest element in array
  */
-const findSmallest = ( arr ) => {
-    let smallest = arr[0];
-    let smallestIndex = 0;
+const find = ( arr, mod ) => {
+    let currentValue = arr[0];
+    let currentValueIndex = 0;
     let arrLen = arr.length;
 
     for ( let i = 0; i < arrLen; i++ ) {
-        if ( arr[i] < smallest ) {
-            smallest = arr[i];
-            smallestIndex = i;
+        if ( mod ) {
+            // Find min
+            if ( arr[i] < currentValue ) {
+                currentValue = arr[i];
+                currentValueIndex = i;
+            }
+        } else {
+            // Find max
+            if ( arr[i] > currentValue ) {
+                currentValue = arr[i];
+                currentValueIndex = i;
+            }
         }
     }
-    return smallestIndex;
+
+    return currentValueIndex;
 };
 
 /**
  * Sorts an array of numbers
+ *
  * @param {Array} arr An array of numbers
+ * @param {boolean} mod TRUE for ascending order or FALSE for descending order
+ *
  * @return {Array} New sorted array
  */
-const selectionSort = ( arr ) => {
+const selectionSort = ( arr, mod ) => {
     let arrCopy = arr.slice( arr );
     let arrNew = [];
     let arrLen = arrCopy.length;
 
     for ( let i = 0; i < arrLen; i++ ) {
-        smallestIndex = findSmallest( arrCopy );
-        arrNew.push( arrCopy.splice( smallestIndex, 1 )[0] );
+        currentIndex = find( arrCopy, mod );
+        arrNew.push( arrCopy.splice( currentIndex, 1 )[0] );
     }
 
     return arrNew;
@@ -37,4 +53,5 @@ const selectionSort = ( arr ) => {
 
 let arr = [5, 3, 6, 2, 10];
 
-console.log( selectionSort(arr) );
+console.log( selectionSort( arr, true ) );
+console.log( selectionSort( arr, false ) );
