cd90cf959b3e51c03a608a681179adea6bae3b3e
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-26 13:43:49 +0300

    Fixed format

--- searching/binary_search.js
+++ searching/binary_search.js
@@ -5,23 +5,23 @@
  * @return {(number | null)} Number if the value is found or NULL otherwise
  */
 const binarySearch = ( list, item ) => {
-    let low = 0;
-    let high = list.length - 1;
+	let low = 0;
+	let high = list.length - 1;
 
-    while ( low <= high ) {
-        let mid = Math.floor((low + high) / 2);
-        let guess = list[mid];
+	while ( low <= high ) {
+		let mid = Math.floor((low + high) / 2);
+		let guess = list[mid];
 
-        if ( guess === item ) {
-            return mid;
-        } else if ( guess > item ) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-        }
-    }
+		if ( guess === item ) {
+			return mid;
+		} else if ( guess > item ) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
 
-    return null;
+	return null;
 };
 
 const myList = [1, 3, 5, 7, 9];
